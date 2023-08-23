5402d1c0589dead4a170496b5d28158e80553a39
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-08-23 20:13:26 +0800

    Edited Contacts Data

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import contactsData from "../data/contactsData";
 
 const Contacts = () => {
   return (
     <div className="contacts">
       <h3>Contacts</h3>
-      <div className="contacts-data">
+      <div className="contacts-data" >
         {contactsData.map((x) =>
           x.link ? (
             <a
@@ -14,10 +15,14 @@ const Contacts = () => {
               key={x.id}
               className="link-style"
             >
-              <p>&nbsp; &#x2022; &nbsp; {x.name}</p>
+              <p>
+                &nbsp; {React.createElement(x.icon)} &nbsp; {x.name}
+              </p>
             </a>
           ) : (
-            <p key={x.id}>&nbsp; &#x2022; &nbsp; {x.name}</p>
+            <p key={x.id}>
+              &nbsp; {React.createElement(x.icon)} &nbsp; {x.name}
+            </p>
           )
         )}
       </div>
--- src/data/contactsData.ts
+++ src/data/contactsData.ts
@@ -1,17 +1,32 @@
-export default [
-    {
-        id: 1,
-        name: 'LinkedIn',
-        link: 'https://www.linkedin.com/in/bryan-mendoza-717357278'
-    },
-    {
-        id: 2,
-        name: 'bryanbenchmarkmendoza@gmail.com',
-        link: ''
-    },
-    {
-        id: 3,
-        name: '09998017946',
-        link: ''
-    }
-]
+import React from 'react';
+import { AiOutlineLinkedin, AiOutlineMail, AiOutlinePhone } from 'react-icons/ai';
+
+interface ContactItem {
+  id: number;
+  name: string;
+  link: string;
+  icon: React.ComponentType; // Use React element type for icons
+}
+
+const contactsData: ContactItem[] = [
+  {
+    id: 1,
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/bryan-mendoza-717357278',
+    icon: AiOutlineLinkedin,
+  },
+  {
+    id: 2,
+    name: 'bryanbenchmarkmendoza@gmail.com',
+    link: '',
+    icon: AiOutlineMail,
+  },
+  {
+    id: 3,
+    name: '09998017946',
+    link: '',
+    icon: AiOutlinePhone,
+  }
+];
+
+export default contactsData;
