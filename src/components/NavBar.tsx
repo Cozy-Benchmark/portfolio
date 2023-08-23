7fccfeb940d7aa37a1db1c5dc71262e686067cf9
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-08-23 11:50:17 +0800

    Removed Contacts Modal

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import ContactsModal from "./ContactsModal";
 
 const NavBar = () => {
-  const [isModalOpen, setModalState] = useState(false);
-  const toggleModal = () => setModalState(!isModalOpen);
-
   return (
     <>
       <div className="container, navbar-box">
@@ -16,10 +11,7 @@ const NavBar = () => {
           <Link to={"/projects"} className="link-style">
             Projects
           </Link>
-          <div onClick={toggleModal} style={{ cursor: "pointer" }}>
-            Contacts
-          </div>
-          <ContactsModal isOpen={isModalOpen} onClose={toggleModal} />
+          <div>Contacts</div>
         </div>
       </div>
     </>
