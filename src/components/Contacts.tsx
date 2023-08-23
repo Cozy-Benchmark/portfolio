0cb41d509267656e248f0aa996b34dfbc64cf5a2
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-08-23 11:52:33 +0800

    Edited contacts display

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -5,9 +5,21 @@ const Contacts = () => {
     <div className="contacts">
       <h3>Contacts</h3>
       <div className="contacts-data">
-        {contactsData.map((x) => (
-          <p>&nbsp; &#x2022; &nbsp; {x.name} </p>
-        ))}
+        {contactsData.map((x) =>
+          x.link ? (
+            <a
+              href={x.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={x.id}
+              className="link-style"
+            >
+              <p>&nbsp; &#x2022; &nbsp; {x.name}</p>
+            </a>
+          ) : (
+            <p key={x.id}>&nbsp; &#x2022; &nbsp; {x.name}</p>
+          )
+        )}
       </div>
     </div>
   );
