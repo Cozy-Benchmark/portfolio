52a79ff45ab2df96dcb39b7ab914d7693e263d56
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-08-23 11:42:24 +0800

    Moved project page css to app.css

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -3,30 +3,31 @@ import projectsData from "../data/projectsData";
 
 const ProjectsPage = () => {
   return (
-    <div style={{ border: "0", padding: "0 10px", marginTop: "40px" }}>
+    <div className="container, projects-screen">
       <NavBar></NavBar>
-
-      <h1>Projects</h1>
-      {projectsData.map((item) => (
-        <>
-          <ul>
-            <h2 id={item.id}>{item.name}</h2>
+      <div className="project-page">
+        <h1>Projects</h1>
+        {projectsData.map((item) => (
+          <>
             <ul>
-              <div>
-                <h3>Description:</h3> <p>{item.descriptions}</p>
-              </div>
-              <div>
-                <h3>Tools Used:</h3>
-                {item.toolsUsed.map((x) => (
-                  <ul>
-                    <li>{x}</li>
-                  </ul>
-                ))}
-              </div>
+              <h2 id={item.id}>{item.name}</h2>
+              <ul>
+                <div>
+                  <h3>Description:</h3> <p>{item.descriptions}</p>
+                </div>
+                <div>
+                  <h3>Tools Used:</h3>
+                  {item.toolsUsed.map((x) => (
+                    <ul>
+                      <li>{x}</li>
+                    </ul>
+                  ))}
+                </div>
+              </ul>
             </ul>
-          </ul>
-        </>
-      ))}
+          </>
+        ))}
+      </div>
     </div>
   );
 };
