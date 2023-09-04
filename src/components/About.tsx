444bd782e30275f3017d5ad01d5b4996ec56bec0
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-09-04 20:52:36 +0800

    Modified About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,17 +2,19 @@ import baguioCityImage from "../assets/general-img/baguio-city.jpg";
 
 const About = () => {
   return (
-    <div className="about">
-      <div className="about-details">
-        <h1 style={{color:'#36f336'}}>About</h1>
-        <p>
-          I’m a fresh BSCS Graduate from Baguio City, Philippines, and an
-          aspiring full-stack software developer. I enjoy building applications
-          from start to finish. Still learning and growing, and hopefully with
-          you!
-        </p>
-      </div>
+    <div className="about-container">
+      <div className="about">
+        <div className="about-details">
+          <h1 style={{color:'#36f336'}}>About</h1>
+          <p style={{textAlign: 'justify', padding: '0 10px'}}>
+            I’m a fresh BSCS Graduate from Baguio City, Philippines, and an
+            aspiring full-stack software developer. I enjoy building applications
+            from start to finish. Still learning and growing, and hopefully with
+            you!
+          </p>
+        </div>
       <img src={baguioCityImage} />
+      </div>
     </div>
   );
 };
