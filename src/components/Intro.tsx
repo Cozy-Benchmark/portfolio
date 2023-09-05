30cea6ccedd17f9396bd72959dbd38eb9a29df69
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-09-05 18:36:32 +0800

    Added Animation and Observer to Intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useRef, useState } from "react";
+
 const Intro = () => {
+  const introRef = useRef<HTMLDivElement>(null);
+  const [introElementVisible, setIntroElementVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        setIntroElementVisible(entry.isIntersecting);
+      },
+      {
+        threshold: 0,
+      }
+    );
+
+    if (introRef.current) {
+      observer.observe(introRef.current);
+    }
+  });
+
   return (
-    <div className="intro">
+    <div
+      className={`intro ${introElementVisible ? "animate" : ""}`}
+      ref={introRef}
+    >
       <h1 className="main">Bryan Benchmark Mendoza</h1>
       <span className="sub-title">An Aspring Software Developer</span>
     </div>
