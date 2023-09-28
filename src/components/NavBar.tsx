b4c9ecc8f854acb512793edaf7dddb37c3e57990
Bryan Benchmark Mendoza <bryanbenchmarkmendoza@gmail.com>
2023-09-28 10:02:27 +0800

    Media Query 320 max width

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,45 @@
 import { HashLink } from "react-router-hash-link";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [clicked, setClick] = useState(false);
+
+  const handleClick = () => setClick(!clicked);
+  const closeMenu = () => setClick(false);
+
   return (
     <>
       <div className="navbar-box">
         <HashLink to={"/#intro"} className="secondary-color link-style">
           Bryan Benchmark Mendoza
         </HashLink>
-        <div className="navbar-options">
-          <a className="navbar-hamburger-menu">
-            <GiHamburgerMenu />
-          </a>
-          <div className="navbar-links">
-            <HashLink to={"/#about"} className="secondary-color link-style">
+        <div className="navbar-options" onClick={handleClick}>
+          <div className="navbar-menu">
+            {clicked ? <AiOutlineClose /> : <GiHamburgerMenu />}
+          </div>
+
+          <div className={clicked ? "navbar-links active" : "navbar-links"}>
+            <HashLink
+              to={"/#about"}
+              className="secondary-color link-style"
+              onClick={closeMenu}
+            >
               About
             </HashLink>
-            <HashLink to={"/#projects"} className="secondary-color link-style">
+            <HashLink
+              to={"/#projects"}
+              className="secondary-color link-style"
+              onClick={closeMenu}
+            >
               Projects
             </HashLink>
-            <HashLink to={"/#contacts"} className="secondary-color link-style">
+            <HashLink
+              to={"/#contacts"}
+              className="secondary-color link-style"
+              onClick={closeMenu}
+            >
               Contacts
             </HashLink>
           </div>
